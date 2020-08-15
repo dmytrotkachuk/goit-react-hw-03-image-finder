54f1878efa79abf8bdea563e10002a10698c5500
Dmytro Tkachuk <tkachukdimon@gmail.com>
2020-08-15 15:18:59 +0300

    Updates

---
+++ precache-manifest.5fcaf02298f2c389da60621b487b2fcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edbab38fa44612f175eb3ca07ff35b4b",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "ffc24180e108140d14a9",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.cf1392b3.chunk.css"
+  },
+  {
+    "revision": "2f0f2d1d746da0f0438c",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.c2f698c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.c2f698c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffc24180e108140d14a9",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.d6efa8b1.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
